a34ce08ad58b2d25d01619cbede9be2c1e1a466e
snake <snake091621@gmail.com>
2026-06-12 23:30:06 +0330

    Update dinosaur.py

--- dinosaur.py
+++ dinosaur.py
@@ -1,12 +1,11 @@
-#pip install pyautogui
 from pyautogui import *
 from time import *
+
 sleep(1)
 
 while 1:
-    #point coordinates (mouse)
     a=position()
-    #point color (RGB)
     b=pixel(a[0],a[1])
+    
     if b==(172, 172, 172):
         press('space')
